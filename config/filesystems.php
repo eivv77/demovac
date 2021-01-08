bd8f35de0a9498177e595631f24bd26104d54545
Zek <zekaabdinov91@gmail.com>
2021-01-08 16:54:57 +0400

    fix

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,20 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => 'local' , //env('FILESYSTEM_DRIVER', 'public'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Cloud Filesystem Disk
+    |--------------------------------------------------------------------------
+    |
+    | Many applications store files both locally and in the cloud. For this
+    | reason, you may specify a default "cloud" driver here. This driver
+    | will be bound as the Cloud disk implementation in the container.
+    |
+    */
+
+    'cloud' => env('FILESYSTEM_CLOUD', 's3'),
 
     /*
     |--------------------------------------------------------------------------
@@ -49,7 +62,6 @@ return [
             'region' => env('AWS_DEFAULT_REGION'),
             'bucket' => env('AWS_BUCKET'),
             'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
         ],
 
     ],
@@ -65,8 +77,8 @@ return [
     |
     */
 
-//    'links' => [
-//        public_path('storage') => storage_path('app/public'),
-//    ],
+    'links' => [
+        public_path('storage') => storage_path('app'),
+    ],
 
 ];
